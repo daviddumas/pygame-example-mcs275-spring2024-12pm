00dc3608407e725bed3451698fd5d64e176d418d
David Dumas <david@dumas.io>
2024-04-24 12:15:25 -0500

    Move images to assets directory

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("Player.png")
+        self.image = pygame.image.load("assets/Player.png")
         self.image.set_colorkey(WHITE)
         self.rect = (
             self.image.get_rect()
@@ -66,7 +66,7 @@ class Robot(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("RobotNPC.png")
+        self.image = pygame.image.load("assets/RobotNPC.png")
         self.image.set_colorkey(WHITE)
         self.rect = (
             self.image.get_rect()
